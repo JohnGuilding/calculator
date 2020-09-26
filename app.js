6ed67bac7d95a4b62567388bd5c79f3548dcceff
John Guilding <jwguilding@gmail.com>
2020-09-26 19:05:35 +0100

    calculator styles finished

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('hi');
+// The challenge is to build a calculator that has the same functionality as your iPhone calculator (portrait). You'll need the following to complete it:
+
+// 1. Conditional Statement (If/Else as well as the &&/|| operators)
+// 2. JS Functions (arrow functions covered this morning)
+// 3. How to add event listeners (interacting with the DOM, covered fully on Thursday but covered a bit already with the validation lesson and responsiveness lesson from last week)
+// 4. How to change the text with the (textContext)
+// 5. For loops (to be covered tomorrow morning but you may have already come across them too)
+
+// +	Addition
+// -	Subtraction
+// *	Multiplication
+// **	Exponentiation (ES2016)
+// /	Division
+// %	Modulus (Remainder)
+// ++	Increment
+// --	Decrement
